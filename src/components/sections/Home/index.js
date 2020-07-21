2cb1acad643818e2fc4be7f9f33a57b920761f1c
Raphael Brandão <raphael.b.souza@hotmail.com>
2020-07-21 10:43:20 -0300

    add section Home

---
+++ src/components/sections/Home/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Grid, Typography, useTheme, useMediaQuery } from '@material-ui/core';
+
+import Image from './Image';
+
+function Home() {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+
+  const nameHeight = '140px';
+  const imageHeight = `calc(100% - ${nameHeight})`;
+
+  const nameStyles = {
+    maxHeight: isDesktop ? 'inherit' : nameHeight,
+    height: isDesktop ? '100%' : 'auto',
+  };
+  const nameAlign = isDesktop ? 'center' : 'left';
+
+  const imageStyles = {
+    height: isDesktop ? '100%' : imageHeight,
+    border: '1px solid grey',
+    padding: 0,
+  };
+
+  return (
+    <Grid container spacing={0} style={{ height: '100%', margin: 0 }}>
+      <Grid
+        item
+        container
+        xs={12}
+        md={4}
+        style={nameStyles}
+        alignContent="center"
+      >
+        <Typography variant="h1" align={nameAlign}>
+          Deborah Brandão
+        </Typography>
+      </Grid>
+      <Grid item xs={12} md={8} style={imageStyles}>
+        <Image />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Home;
